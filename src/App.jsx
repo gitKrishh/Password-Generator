34c80984d4ab751dbd4ee03435ac047752dc3841
gitkrishh <msgkrish192@gmail.com>
2025-05-03 16:18:31 +0530

    added logic for generating password

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setlength] = useState(8);
+  const [numberallow, setnumberallow] = usestate(false);
+  const [spcharallow, setspcharallow] = useState(false);
+  const [pass, setpass] = useState("");
 
+  const passGenerator = useCallback(()=>{
+    let pass="";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberallow) str+="0123456789";
+    if(spcharallow) str+="!@#$%^&*()-_=+\|[]{};:/?.> ;"
+    
+    for(let i=1; i <= array.length; i++){
+      let char= Math.floor(Math.random()*str.length +1); //math.floor round of the number and math.random generat the random decima number between 0 to 1, and str.length 
+      pass=str.charAt(char); //str.charat(index) return the specific(index) char from the str 
+      
+    }
+    setpass(pass);
+  }, [length, numberallow, spcharallow, setpass])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
     </>
   )
 }
